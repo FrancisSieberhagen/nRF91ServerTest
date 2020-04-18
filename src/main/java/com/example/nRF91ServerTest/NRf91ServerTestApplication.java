f2754ec211fe64a5cdb190bd36876b917806c9a7
root <root@li1973-205.members.linode.com>
2020-04-18 09:24:55 +0000

    Initial commit

---
+++ src/main/java/com/example/nRF91ServerTest/NRf91ServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.nRF91ServerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NRf91ServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NRf91ServerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nRF91ServerTest/NRf91ServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nRF91ServerTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NRf91ServerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
