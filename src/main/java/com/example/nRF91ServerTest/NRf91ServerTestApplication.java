543f48aee17b90fd0c2246737f577f28cb26490f
root <root@li1973-205.members.linode.com>
2020-04-18 09:33:08 +0000

    Initial commit

--- src/main/java/com/example/nRF91ServerTest/NRf91ServerTestApplication.java
+++ src/main/java/com/example/nRF91ServerTest/NRf91ServerTestApplication.java
@@ -3,6 +3,10 @@ package com.example.nRF91ServerTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 @SpringBootApplication
 public class NRf91ServerTestApplication {
 
@@ -10,4 +14,13 @@ public class NRf91ServerTestApplication {
 		SpringApplication.run(NRf91ServerTestApplication.class, args);
 	}
 
+}
+
+@RestController
+class Action {
+
+    @RequestMapping("/")
+    String index() {
+        return "{\"ActionName\":\"BSD Test\",\"LED1\":true,\"LED2\":true}";
+    }
 }
